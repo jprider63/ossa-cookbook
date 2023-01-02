f1f81bb8d55873ae22cb19a070501b2b6727827e
James Parker <dev@jamesparker.me>
2023-01-02 02:41:44 -0500

    Views

--- src/gui/layout.rs
+++ src/gui/layout.rs
@@ -1,10 +1,65 @@
+use dioxus::events::MouseEvent;
 use dioxus::prelude::*;
 use dioxus_heroicons::{Icon, solid::Shape};
 // TODO: Fix outline icons.
 
-// use crate::gui::layout::sidebar_header;
+use crate::state::{Cookbook, CookbookId};
 
-pub fn layout(cx: Scope) -> Element {
+enum View {
+    Login,
+    NoSelection,
+    Cookbook(CookbookId),
+}
+
+#[inline_props]
+pub fn layout<'a>(cx: Scope, state: &'a UseState<Vec<Cookbook>>) -> Element {
+    let view = use_state(&cx, || {
+        View::NoSelection
+    });
+
+    match *view.current() {
+        View::Login => todo!(),
+        View::NoSelection => cx.render(rsx! ( NoSelectionView { view: view, state: state } )),
+        View::Cookbook(cookbookid) => cx.render(rsx! ( CookbookView { view: view, state: state, cookbook_id: cookbookid } )),
+    }
+}
+
+#[inline_props]
+fn NoSelectionView<'a>(cx: Scope, view: &'a UseState<View>, state: &'a UseState<Vec<Cookbook>>) -> Element {
+    cx.render(rsx! (
+        Sidebar { view: view, state: state }
+    ))
+}
+
+#[inline_props]
+fn CookbookView<'a>(cx: Scope, view: &'a UseState<View>, state: &'a UseState<Vec<Cookbook>>, cookbook_id: CookbookId) -> Element {
+    if let Some(cookbook) = state.current().get(*cookbook_id) {
+        println!("rendering {}..", cookbook.title);
+        cx.render(rsx! (
+            Sidebar { view: view, state: state }
+            div {
+                class: "min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased",
+                "{cookbook.title}"
+            }
+        ))
+    } else {
+        // Cookbook not found, so set no selection.
+        // TODO: Log this.
+        println!("Cookbook {} not found.", cookbook_id);
+
+        view.set(View::NoSelection);
+        cx.render(rsx! (
+            ""
+        ))
+    }
+}
+
+#[inline_props]
+fn Sidebar<'a>(cx: Scope, view: &'a UseState<View>, state: &'a UseState<Vec<Cookbook>>) -> Element {
+    let cookbooks = state.iter().enumerate().map(|(i,cookbook)| rsx!(
+        SidebarItem { title: &cookbook.title, icon: Shape::BookOpen, onclick: move |_e| {view.set(View::Cookbook(i))} }
+    ));
+
     cx.render(rsx! (
         div {
             class: "min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800",
@@ -15,17 +70,20 @@ pub fn layout(cx: Scope) -> Element {
                     ul {
                         class: "flex flex-col py-4 space-y-1",
                         SidebarHeader { title: "COOKBOOKS" }
-                        SidebarItem   { title: "Family Recipes", icon: Shape::BookOpen }
-                        SidebarItem   { title: "My Recipes", icon: Shape::BookOpen }
+                        cookbooks
                         SidebarHeader { title: "MEAL PLANNER" }
-                        SidebarItem   { title: "Weekly meals", icon: Shape::PencilSquare }
-                        SidebarItem   { title: "Thanksgiving", icon: Shape::PencilSquare }
+                        SidebarItem   { title: "Weekly meals", icon: Shape::PencilSquare, onclick: |_e| {println!("TODO!")} }
+                        SidebarItem   { title: "Thanksgiving", icon: Shape::PencilSquare, onclick: |_e| {println!("TODO!")} }
                         SidebarHeader { title: "SETTINGS" }
-                        SidebarItem   { title: "Account", icon: Shape::User }
-                        SidebarItem   { title: "Logout", icon: Shape::ArrowRightOnRectangle }
+                        SidebarItem   { title: "Account", icon: Shape::User, onclick: |_e| {println!("TODO!")} }
+                        SidebarItem   { title: "Logout", icon: Shape::ArrowRightOnRectangle, onclick: |_e| {println!("TODO!")} }
                     }
                 }
             }
+            // div {
+            //     class: "flex flex-col",
+            //     "TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            // }
         }
     ))
 }
@@ -54,6 +112,7 @@ pub fn SidebarHeader<'a>(cx: Scope<'a, SidebarHeaderProps<'a>>) -> Element {
 pub struct SidebarItemProps<'a> {
     title: &'a str,
     icon: Shape,
+    onclick: EventHandler<'a, MouseEvent>,
 }
 
 pub fn SidebarItem<'a>(cx: Scope<'a, SidebarItemProps<'a>>) -> Element {
@@ -62,6 +121,7 @@ pub fn SidebarItem<'a>(cx: Scope<'a, SidebarItemProps<'a>>) -> Element {
             a {
                 class: "relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6",
                 href: "#",
+                onclick: |e| cx.props.onclick.call(e),
                 span {
                     class: "inline-flex justify-center items-center ml-4",
                     Icon {
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use dioxus::prelude::*;
+use crate::state::*;
 
 mod gui;
+mod state;
 
 fn main() {
     dioxus::desktop::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
+    let state = use_state(&cx, || {
+        let book1 = Cookbook {title: "Family Recipes".into(), recipes: vec![]};
+        let book2 = Cookbook {title: "My Recipes".into(), recipes: vec![]};
+        vec![book1, book2]
+        // TODO: Should be a Map CRDT. Include other store metadata like sharing/permissions, peers, etc
+    });
+
     cx.render(rsx! (
         style { [include_str!("../dist/style.css")] }
 
         rsx! (
-            gui::layout::layout()
+            gui::layout::layout { state: state }
         )
     ))
 }
